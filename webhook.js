f15f1e51740bbcc6373fb842b12fa4b8a530eed1
guihubb <guilhermetaborda1907@gmail.com>
2026-07-17 12:25:10 -0300

    Voltando a utilizar o módulo axios

--- webhook.js
+++ webhook.js
@@ -1,50 +1,60 @@
-//const axios = require('axios');
-const bodyParser = require('body-parser')
-const http = require('http');
-const express = require('express');
-const app = express();
+const axios = require('axios');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extende: true}))
 
-const server = http.createServer((req, res) => {
-    res.write('Hello World')
-    res.end()
-});
+const token = "EAAUi1ZAjBIQwBR0ZCJMMrZCaZAkHZC8ZC9GUVSePNUwzDZAOO7fsGkGOFCJXRKo0nWwas8dni7NSQJ5bRWXw9r2lkDlnmIDZA2ocP7CGfIZCDo3v6sn8vl7gRcTxFZBGWzRMwGG1rSgXdBNOXyK5oBKPpIdqQRXKo1pPtTCxz4ocm6b3ToOPGVN2UsTVmkQJQHTgZDZD";
+const idPhoneNumber = "436813806185181";
+const destinatario = "5541992314305"
 
-server.listen(85, () => {
-    console.log("Servidor rodando na porta 85")
-})
-
-const messages = require('./includes/messages')
-const functions = require('./includes/functions')
-
-global.token = "EAAUi1ZAjBIQwBR0ZCJMMrZCaZAkHZC8ZC9GUVSePNUwzDZAOO7fsGkGOFCJXRKo0nWwas8dni7NSQJ5bRWXw9r2lkDlnmIDZA2ocP7CGfIZCDo3v6sn8vl7gRcTxFZBGWzRMwGG1rSgXdBNOXyK5oBKPpIdqQRXKo1pPtTCxz4ocm6b3ToOPGVN2UsTVmkQJQHTgZDZD";
-global.idPhoneNumber = "436813806185181";
-global.urlBase =  "https://graph.facebook.com/v25.0/"+idPhoneNumber+"/messages"
-global.usersActive = [];
-global.msgActive = false;
-
-//const destinatario = "5541992314305";
-
-app.post('/webhook', (req, res) => {
-    messageData = functions.getMessageData(req)
-
-    if(messageData) {
-        msgActive = false;
-        messages.markAsRead(req, res, messageData);
-        msgId = messageData.msgId;
-        msgFromNumber = messageData.msgFromNumber;
-        msgFromName = messageData.msgFromName;
-
-        if (messageData.msgType == "text") {
-            if (msgActive == true) {
-                messages.markAsRead(req, res, messageData);
-                messages.sendToken(req, res, messageData);
+const enviarMensagem = async () => {
+    try {
+        const response = await axios.post(
+            `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
+            {
+                messaging_product: 'whatsapp',
+                recipient_type: 'individual',
+                to: destinatario,
+                type: 'template',
+                //type: 'text',
+                template: {
+                    'name': 'envio_token',
+                    'language': {
+                        code: 'pt_BR'
+                    },
+                    components: [
+                        {
+                            'type': 'body',
+                            'parameters': [
+                                {
+                                    'type': 'text',
+                                    'text': '12345'
+                                }
+                            ]
+                        },
+                        {
+                            'type': 'button',
+                            'sub_type': 'url',
+                            'index': '0',
+                            'parameters': [
+                                {
+                                    'type': 'text',
+                                    'text': '12345'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
             }
-        }
+        );
+        console.log('Mensagem enviada!', response.data)
+    } catch (err) {
+        console.log('Erro ao enviar a mensagem: ', err.response ? err.response.data : err.message);
     }
-})
+}
 
-app.get('/webhook', (req, res) => {res.status(404).send()});
-app.post('/webhook', (req, res) => {res.status(404).send()})
+enviarMensagem();
