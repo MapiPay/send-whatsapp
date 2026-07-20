67512bbdf465419c1e7dac83b7616654c6ac9136
guihubb <guilhermetaborda1907@gmail.com>
2026-07-20 14:55:07 -0300

    Iniciando testes da função IIFE

--- webhook.js
+++ webhook.js
@@ -4,30 +4,34 @@ const axios = require('axios');
 const token = "EAAUi1ZAjBIQwBR0ZCJMMrZCaZAkHZC8ZC9GUVSePNUwzDZAOO7fsGkGOFCJXRKo0nWwas8dni7NSQJ5bRWXw9r2lkDlnmIDZA2ocP7CGfIZCDo3v6sn8vl7gRcTxFZBGWzRMwGG1rSgXdBNOXyK5oBKPpIdqQRXKo1pPtTCxz4ocm6b3ToOPGVN2UsTVmkQJQHTgZDZD";
 const idPhoneNumber = "436813806185181";
 const destinatario = "5541992314305";
+//const messageId = 'wamid.HBgMNTU0MTkyMzE0MzA1FQIAEhggQUM2RThEQTlCQzBGOEI0Qjc5RjkzN0QxMUY3MTg0OTUA';
 
-(function (msgId) {
+(async function (msgId) {
+    const url = `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`;
+
+    const payload = {
+        'messaging_product': 'whatsapp',
+        'status': 'read',
+        'message_id': msgId
+    };
+
     try {
-        const response = axios.post(
-            `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
-            {
-                'messaging_product': 'whatsapp',
-                'status': 'read',
-                'message_id': msgId
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    'Content-Type': 'application/json'
-                }
+        const response = await axios.post(url, payload, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
             }
-        )
+        });
+
         console.log('Mensagem marcada como lida!', response.data);
-    } catch(err){
+        return response.data;
+    } catch (err) {
         console.log('Erro ao enviar a mensagem: ', err.response ? err.response.data : err.message);
     }
-})()
 
-async function enviarMensagem() {
+})(messageId)
+
+/*async function enviarMensagem() {
     try {
         const response = await axios.post(
             `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
@@ -78,4 +82,4 @@ async function enviarMensagem() {
         console.log('Erro ao enviar a mensagem: ', err.response ? err.response.data : err.message);
     }
 }
-enviarMensagem();
+enviarMensagem();*/
