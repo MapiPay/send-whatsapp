23847ad9ce26d31c9aef8c2f7cc9a327545aeb2c
guihubb <guilhermetaborda1907@gmail.com>
2026-07-20 14:05:00 -0300

    Correção na função IIFE

--- webhook.js
+++ webhook.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 
 const token = "EAAUi1ZAjBIQwBR0ZCJMMrZCaZAkHZC8ZC9GUVSePNUwzDZAOO7fsGkGOFCJXRKo0nWwas8dni7NSQJ5bRWXw9r2lkDlnmIDZA2ocP7CGfIZCDo3v6sn8vl7gRcTxFZBGWzRMwGG1rSgXdBNOXyK5oBKPpIdqQRXKo1pPtTCxz4ocm6b3ToOPGVN2UsTVmkQJQHTgZDZD";
 const idPhoneNumber = "436813806185181";
-const destinatario = "5541992314305"
+const destinatario = "5541992314305";
 
-(() => {
+(function() {
     axios.post(
         `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
         {
