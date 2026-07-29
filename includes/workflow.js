f7a54d47531de12cf0df06b9e7301eae78932049
guihubb <guilhermetaborda1907@gmail.com>
2026-07-29 14:07:42 -0300

    redefinindo bloco try

--- includes/workflow.js
+++ includes/workflow.js
@@ -25,22 +25,68 @@ async function solicitarDocumento(destinatario) {
 }
 
 async function iniciarFlow(numCliente, documento) {
+    if (documento.length <= 14) {
+        console.log(`Cliente ${numCliente} iniciou o atendimento para CPF`);
+
+        flowCpf(numCliente);
+        console.log('Início do flow CPF')
+    } else if (documento.length > 14) {
+        console.log(`Cliente ${numCliente} iniciou o atendimento para CNPJ`);
+
+        flowCNPJ(numCliente);
+        console.log('Início do flow CNPJ')
+    } else {
+        console.log("Documento inválido")
+    }
+}
+
+
+async function flowCpf(destinatario) {
     try {
-        if (documento.length <= 14) {
-            console.log(`Cliente ${numCliente} iniciou o atendimento para CPF`);
-
-            flowCpf(numCliente);
-            console.log('Início do flow CPF')
-        } else if (documento.length > 14) {
-            console.log(`Cliente ${numCliente} iniciou o atendimento para CNPJ`);
-
-            flowCNPJ(numCliente);
-            console.log('Início do flow CNPJ')
-        } else {
-            console.log("Documento inválido")
-        }
-    } catch(err){
-        if(err.response){
+        const response = axios.post(
+            `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
+            {
+                "messaging_product": 'whatsapp',
+                "to": destinatario,
+                "type": 'interactive',
+                "interactive": {
+                    "type": 'list',
+                    "header": {
+                        "type": 'text',
+                        "text": 'Atendimento Pessoa Física'
+                    },
+                    "body": {
+                        "text": 'Selecione uma das opções abaixo:'
+                    },
+                    "footer": {
+                        "text": 'Escolha uma opção para continuar',
+                    },
+                    "action": {
+                        "button": 'Ver opções',
+                        "sections": [
+                            {
+                                "title": 'Menu CPF',
+                                "rows": [
+                                    { "id": "pf_1", "title": "1 - Saldo" },
+                                    { "id": "pf_2", "title": "2 - PIX" },
+                                    { "id": "pf_3", "title": "3 - Cartão" },
+                                    { "id": "pf_4", "title": "4 - Benefícios" },
+                                    { "id": "pf_5", "title": "5 - Senha" },
+                                    { "id": "pf_6", "title": "6 - Falar com atendente" }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            },
+            {
+                "headers": {
+                    "Authorization": `Bearer ${token}`
+                }
+            }
+        )
+    } catch (err) {
+        if (err.response) {
             console.log("Erro da Meta:", JSON.stringify(err.response.data, null, 2));
         } else {
             console.log("Erro ao processar o payload da Meta: ", err);
@@ -48,94 +94,57 @@ async function iniciarFlow(numCliente, documento) {
     }
 }
 
-async function flowCpf(destinatario) {
-    const response = axios.post(
-        `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
-        {
-            "messaging_product": 'whatsapp',
-            "to": destinatario,
-            "type": 'interactive',
-            "interactive": {
-                "type": 'list',
-                "header": {
-                    "type": 'text',
-                    "text": 'Atendimento Pessoa Física'
-                },
-                "body": {
-                    "text": 'Selecione uma das opções abaixo:'
-                },
-                "footer": {
-                    "text": 'Escolha uma opção para continuar',
-                },
-                "action": {
-                    "button": 'Ver opções',
-                    "sections": [
-                        {
-                            "title": 'Menu CPF',
-                            "rows": [
-                                { "id": "pf_1", "title": "1 - Saldo" },
-                                { "id": "pf_2", "title": "2 - PIX" },
-                                { "id": "pf_3", "title": "3 - Cartão" },
-                                { "id": "pf_4", "title": "4 - Benefícios" },
-                                { "id": "pf_5", "title": "5 - Senha" },
-                                { "id": "pf_6", "title": "6 - Falar com atendente" }
-                            ]
-                        }
-                    ]
-                }
-            }
-        },
-        {
-            "headers": {
-                "Authorization": `Bearer ${token}`
-            }
-        }
-    )
-}
-
 async function flowCNPJ(destinatario) {
-    const response = axios.post(
-        `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
-        {
-            "messaging_product": "whatsapp",
-            "to": "NUMERO_DO_CLIENTE",
-            "type": "interactive",
-            "interactive": {
-                "type": "list",
-                "header": {
-                    "type": "text",
-                    "text": "Atendimento Pessoa Jurídica"
-                },
-                "body": {
-                    "text": "Selecione uma das opções abaixo:"
-                },
-                "footer": {
-                    "text": "Escolha uma opção para continuar"
-                },
-                "action": {
-                    "button": "Ver opções",
-                    "sections": [
-                        {
-                            "title": "Menu PJ",
-                            "rows": [
-                                { "id": "pj_1", "title": "1 - Financeiro" },
-                                { "id": "pj_2", "title": "2 - PIX" },
-                                { "id": "pj_3", "title": "3 - QR Code / Pagamentos" },
-                                { "id": "pj_4", "title": "4 - Nota Fiscal" },
-                                { "id": "pj_5", "title": "5 - Comercial" },
-                                { "id": "pj_6", "title": "6 - Suporte" }
-                            ]
-                        }
-                    ]
+    try {
+        const response = axios.post(
+            `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`,
+            {
+                "messaging_product": "whatsapp",
+                "to": "NUMERO_DO_CLIENTE",
+                "type": "interactive",
+                "interactive": {
+                    "type": "list",
+                    "header": {
+                        "type": "text",
+                        "text": "Atendimento Pessoa Jurídica"
+                    },
+                    "body": {
+                        "text": "Selecione uma das opções abaixo:"
+                    },
+                    "footer": {
+                        "text": "Escolha uma opção para continuar"
+                    },
+                    "action": {
+                        "button": "Ver opções",
+                        "sections": [
+                            {
+                                "title": "Menu PJ",
+                                "rows": [
+                                    { "id": "pj_1", "title": "1 - Financeiro" },
+                                    { "id": "pj_2", "title": "2 - PIX" },
+                                    { "id": "pj_3", "title": "3 - QR Code / Pagamentos" },
+                                    { "id": "pj_4", "title": "4 - Nota Fiscal" },
+                                    { "id": "pj_5", "title": "5 - Comercial" },
+                                    { "id": "pj_6", "title": "6 - Suporte" }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            },
+            {
+                "headers": {
+                    "Authorization": `Bearer ${token}`
                 }
             }
-        },
-        {
-            "headers": {
-                "Authorization": `Bearer ${token}`
-            }
+        )
+    } catch (err) {
+        if (err.response) {
+            console.log("Erro da Meta:", JSON.stringify(err.response.data, null, 2));
+        } else {
+            console.log("Erro ao processar o payload da Meta: ", err);
         }
-    )
+    }
 }
 
 module.exports = {
