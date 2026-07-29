231062d23b024c692dfd5946662debb2c060531d
guihubb <guilhermetaborda1907@gmail.com>
2026-07-29 13:00:43 -0300

    correção no workflow.js

--- includes/workflow.js
+++ includes/workflow.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const idPhoneNumber = "436813806185181";
+const token = "EAAUi1ZAjBIQwBR0ZCJMMrZCaZAkHZC8ZC9GUVSePNUwzDZAOO7fsGkGOFCJXRKo0nWwas8dni7NSQJ5bRWXw9r2lkDlnmIDZA2ocP7CGfIZCDo3v6sn8vl7gRcTxFZBGWzRMwGG1rSgXdBNOXyK5oBKPpIdqQRXKo1pPtTCxz4ocm6b3ToOPGVN2UsTVmkQJQHTgZDZD";
 
 async function solicitarDocumento(destinatario) {
     await axios.post(
