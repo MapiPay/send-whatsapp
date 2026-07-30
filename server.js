55781ff4838ab93c52459e5f920bf4ce0c8e5fcf
guihubb <guilhermetaborda1907@gmail.com>
2026-07-30 13:59:36 -0300

    redirecionamento para o chat do comercial

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.get('/', (req, res) => {
     return res.sendStatus(400)
 })
 
+app.get('/comercial', (req, res) => {
+    res.redirect('https://wa.me/554191069081?text=Ol%C3%A1!%20Ainda%20n%C3%A3o%20sou%20cliente%20e%20gostaria%20de%20falar%20com%20a%20equipe%20comercial%20da%20MapiPay.')
+})
 
 app.post('/', async (req, res) => {
     const body = req.body;
