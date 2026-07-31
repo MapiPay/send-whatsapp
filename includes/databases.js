8f73683b6c4780ecef21ac39277f7e318593b4b6
guihubb <guilhermetaborda1907@gmail.com>
2026-07-31 17:47:11 -0300

    adição do banco de dados

--- includes/databases.js
+++ includes/databases.js
@@ -1,7 +1,24 @@
 const mysql = require('mysql');
+const request = require('request');
+
 global.connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'serpro',
     password: 'S4rpro@mapi',
     database: 'serpro'
 });
+
+
+module.exports = {
+    getData(req, res) {
+        var documentNumber = req.body.documentNumber;
+        connection.query("SELECT dados FROM consultas WHERE documento = " + documentNumber + ";", function (err, result, fields) {
+            try {
+                results.recordset.forEach(dados => {
+                    retorno = JSON.parse(result[0].dados);
+                    res.send(retorno);
+                })
+            } catch (err) { }
+        })
+    }
+}
