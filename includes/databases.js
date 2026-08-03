7c60448bb39846dbd98a7c5745c770f9eeab2552
guihubb <guilhermetaborda1907@gmail.com>
2026-08-03 15:59:19 -0300

    modificando os logs

--- includes/databases.js
+++ includes/databases.js
@@ -11,7 +11,7 @@ function getData(req, res, documentNumber) {
     // const documentNumber = req.body.documentNumber;
     connection.query("SELECT dados FROM consultas WHERE documento = " + documentNumber + ";", function (err, result, fields) {
         try {
-            result.recordset.forEach(dados => {
+            results.recordset.forEach(dados => {
                 let retorno = JSON.parse(result[0].dados);
                 res.send(retorno);
             })
