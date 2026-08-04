b582eb9525d4608529b88cf9b407f99d32940657
guihubb <guilhermetaborda1907@gmail.com>
2026-08-04 16:59:14 -0300

    Atualização

--- includes/workflow.js
+++ includes/workflow.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const jsons = require('./jsons')
 const query = require('./databases');
+const sRequest = require('sync-request');
 const idPhoneNumber = "436813806185181";
 const token = "EAAUi1ZAjBIQwBR0ZCJMMrZCaZAkHZC8ZC9GUVSePNUwzDZAOO7fsGkGOFCJXRKo0nWwas8dni7NSQJ5bRWXw9r2lkDlnmIDZA2ocP7CGfIZCDo3v6sn8vl7gRcTxFZBGWzRMwGG1rSgXdBNOXyK5oBKPpIdqQRXKo1pPtTCxz4ocm6b3ToOPGVN2UsTVmkQJQHTgZDZD";
 
@@ -30,14 +31,14 @@ async function iniciarFlow(req, res, numCliente, documento) {
     const cpfCnpj = String(documento || '').replace(/\D/g, '');
 
     try {
-        const consulta = query.getData(req, res, documento);
-        console.log(consulta);
+        query.getData(req, res, documento);
         
         if (cpfCnpj.length === 11) {
             console.log(`Cliente ${numCliente} iniciou o atendimento para CPF`);
 
             const jsonPF = jsons.menuCPF(numCliente)
-            
+            const response = sRequest('POST', `https://graph.facebook.com/v19.0/${idPhoneNumber}/messages`, { headers: { 'content-type': 'application/json' }, body: JSON.stringify(jsonPF) });
+            const retorno = JSON.parse(response.body)
             console.log('Início do flow CPF')
         } else if (cpfCnpj.length > 11) {
             console.log(`Cliente ${numCliente} iniciou o atendimento para CNPJ`);
