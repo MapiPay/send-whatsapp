54e617e31ce6ccf8ad72b9ea536145e9340994b3
guihubb <guilhermetaborda1907@gmail.com>
2026-08-06 13:40:56 -0300

    Alteração de função para receber opção do usuário

---
+++ includes/estadoCliente.js
@@ -0,0 +1,21 @@
+const redis = require('redis');
+const client = redis.createClient();
+await client.connect();
+
+async function setEstadoCliente(numCliente, estado) {
+    await client.set(`estado:${numCliente}`, estado, { EX: 600 });
+}
+
+async function getEstadoCliente(numCliente) {
+    return await client.get(`estado:${numCliente}`);
+}
+
+async function clearEstadoCliente(numCliente) {
+    await client.del(`estado:${numCliente}`);
+}
+
+module.exports = { 
+    setEstadoCliente, 
+    getEstadoCliente, 
+    clearEstadoCliente 
+};
