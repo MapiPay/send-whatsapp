d808dbde6498a35884bb035683e03428237c5090
guihubb <guilhermetaborda1907@gmail.com>
2026-08-07 15:47:06 -0300

    separação do database

--- includes/functions.js
+++ includes/functions.js
@@ -80,8 +80,7 @@ const sRequest = require('sync-request');
 function consulta(documento) {
     let reqCliente = sRequest('POST', 'https://serpro.mapipay.com.br/consulta', { headers: { 'content-type': 'application/json' }, body: JSON.stringify({ "documentNumber": documento }) })
     resCliente = JSON.parse(reqCliente.body);
-    const nome = resCliente.nome
-    return nome;
+    return resCliente
 }
 
 
