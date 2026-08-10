6440b52eef048912cd8676fe8b2f344033e8ba29
guihubb <guilhermetaborda1907@gmail.com>
2026-08-10 15:26:45 -0300

    tratando erro na função exibirSaldo

--- includes/jsons.js
+++ includes/jsons.js
@@ -97,7 +97,7 @@ module.exports = {
                 }
             )
         } catch (err) {
-            console.log('Erro ao exibir saldo PF', err)
+            console.log('Erro ao exibir saldo PF', err.response?.data)
         }
     },
 
